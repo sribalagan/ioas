e9cb14cc772bf9f49d152ccce26c7077e77ef06e
sribalagn <sribalagan.21@gmai.com>
2025-01-10 12:25:21 +0530

    fixed

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,16 +2,16 @@ const bcrypt = require("bcryptjs");
 
 // Sample in-memory user data (passwords are hashed using bcrypt)
 const users = [
-  { id: "1", username: "user1", password: bcrypt.hashSync("password1", 10) }, // hashed password
-  { id: "2", username: "user2", password: bcrypt.hashSync("password2", 10) }  // hashed password
+  {username: "user1", password: bcrypt.hashSync("password1", 10) }, // hashed password
+  {username: "user2", password: bcrypt.hashSync("password2", 10) }  // hashed password
 ];
-
+console.log(users[0].password);
 // Simulate login authentication logic with bcrypt password validation
 exports.login = (req, res) => {
-  const { id, username, password } = req.body;
+  const {username, password } = req.body;
 
   // Find the user by id and username
-  const user = users.find(user => user.id === id && user.username === username);
+  const user = users.find(user =>user.username === username);
 
   if (user) {
     // Compare the entered password with the stored hashed password
